d811a3e4933dbb676154e238258525d82d8e19c4
dcsl691215 <792394059@qq.com>
2022-07-27 12:08:58 +0800

    编写了JDBC模块，但没有并入到storm部分

---
+++ src/main/java/myJDBC/Init.java
@@ -0,0 +1,15 @@
+package myJDBC;
+
+public class Init {
+    String entrust_date = "未知";
+    String entrust_time = "未知";
+    String market_sector = "未知";
+    String entrust_account = "未知";
+    String stock_symbol = "未知";
+    String entrust_id = "未知";
+    String entrust_behavior = "未知";
+    String entrust_state = "未知";
+    String entrust_count = "未知";
+    String entrust_prise = "未知";
+    String entrust_amount = "未知";
+}
--- src/main/java/myJDBC/myUtils.java
+++ src/main/java/myJDBC/myUtils.java
@@ -1,20 +1,23 @@
 package myJDBC;
 
+import org.junit.Test;
+
 import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
 
 public class myUtils {
-
     public static Connection getConnection() throws Exception
     {
         Properties pro = new Properties();
-        InputStream is  = myUtils.class.getResourceAsStream("dataBase.properties");
+        InputStream is  = myUtils.class.getClassLoader().getResourceAsStream("dataBase.properties");
+        System.out.println(is);
         pro.load(is);
         String url = pro.getProperty("jdbc.url");
-        String Driver = pro.getProperty("jdbc.Driver");
+        String Driver = pro.getProperty("jdbc.driver");
         String username = pro.getProperty("jdbc.username");
         String password = pro.getProperty("jdbc.password");
         Class.forName(Driver);
@@ -23,11 +26,28 @@ public class myUtils {
     }
     public static void InsertResult(Result result) throws Exception {
         Connection conn = getConnection();
-        String insertSQL = "insert into Result values ("+result.exception_scenarios+","+result.exception_date+","+result.exception_time+","+result.market_sector+","+result.entrust_account+","+result.stock_symbol+","+result.description+","+result.Risk_level+")";
+        String insertSQL = "insert into result values ("+"\'"+result.exception_scenarios+"\'"+","+"\'"+result.exception_date+"\'"+","+"\'"+result.exception_time+"\'"+","+"\'"+result.market_sector+"\'"+","+"\'"+result.entrust_account+"\'"+","+"\'"+result.stock_symbol+"\'"+","+"\'"+result.description+"\'"+","+"\'"+result.Risk_level+"\'"+")";
         Statement st = conn.createStatement();
         st.executeUpdate(insertSQL);
         System.out.println("插入一条数据");
         conn.close();
 
+    }
+    public static void InsertInit(Init init) throws Exception {
+        Connection conn = getConnection();
+        String insertSQL = "insert into entrust values ("+"\'"+init.entrust_date+"\'"+","+"\'"+init.entrust_time+"\'"+","+"\'"+init.market_sector+"\'"+","+"\'"+init.entrust_account+"\'"+","+"\'"+init.stock_symbol+"\'"+","+"\'"+init.entrust_id+"\'"+","+"\'"+init.entrust_behavior+"\'"+","+"\'"+init.entrust_state+"\'"+","+"\'"+init.entrust_count+"\'"+","+"\'"+init.entrust_prise+"\'"+","+"\'"+init.entrust_amount+"\'"+")";
+        Statement st = conn.createStatement();
+        st.executeUpdate(insertSQL);
+        System.out.println("插入一条数据");
+        conn.close();
+    }
+    public static void Select(String tableName) throws Exception {
+        Connection conn = getConnection();
+        String selectSQL = "select * from " + tableName;
+        Statement st = conn.createStatement();
+        ResultSet resultSet = st.executeQuery(selectSQL);
+        if(resultSet.next())
+            System.out.println(resultSet.getString("stock_symbol"));
+        conn.close();
     }
 }
